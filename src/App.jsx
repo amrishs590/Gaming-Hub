8283365934c77dda9c2dd166b51f8075f4882329
amrishs590 <amrishs590@gmail.com>
2025-06-24 00:27:34 +0530

    Stylling applied for mini games

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import TicTacToe from "./games/TicTacToe";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import TicTacToe from './games/TicTacToe';
+import RockPaperScissors from './games/RockPaperScissors';
+import Snake from './games/Snake';
+import MemoryMatch from './games/MemoryMatch';
+import WhackAMole from './games/WhackAMole';
 
 const App = () => {
   return (
@@ -8,6 +12,10 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/games/tic-tac-toe" element={<TicTacToe />} />
+        <Route path="/games/rock-paper-scissors" element={<RockPaperScissors />} />
+        <Route path="/games/snake" element={<Snake />} />
+        <Route path="/games/memory-match" element={<MemoryMatch />} />
+        <Route path="/games/whack-a-mole" element={<WhackAMole />} />
       </Routes>
     </Router>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,16 @@
 import { Link } from "react-router-dom";
-import './Home.css';
+import "./Home.css";
 
 const Home = () => {
   return (
     <div className="home-container">
       <h1>🎮 Gaming Hub</h1>
       <div className="games-grid">
-        <Link to="/games/tic-tac-toe" className="game-card">Tic Tac Toe</Link>
-        <Link to="/games/rock-paper-scissor" className="game-card">Rock Paper Scissor</Link>
+        <Link to="/games/tic-tac-toe">Tic Tac Toe</Link>
+        <Link to="/games/rock-paper-scissors">Rock Paper Scissors</Link>
+        <Link to="/games/snake">Snake</Link>
+        <Link to="/games/memory-match">Memory Match</Link>
+        <Link to="/games/whack-a-mole">Whack-a-Mole</Link>
       </div>
     </div>
   );
